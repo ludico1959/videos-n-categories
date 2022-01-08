c160a52e1c9e45a13fb9a4a0f56aff90249bc07e
Lucas Dias Coelho <lucas.coelho@compasso.com.br>
2022-01-08 14:31:54 -0300

    Add create and get all rountes for videos

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { CreateCategoryController } from "./controllers/CreateCategoryController
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
+import { CreateVideoController } from "./controllers/CreateVideoController";
+import { GetAllVideosController } from "./controllers/GetAllVideosController";
 
 const routes = Router();
 
@@ -10,5 +12,8 @@ routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
+
+routes.post("/videos", new CreateVideoController().handle);
+routes.get("/videos", new GetAllVideosController().handle);
 
 export { routes };
